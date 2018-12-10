38f0093db4337f657e54c444d96c5f67e8dc37f9
Chanchal Kumar Maji <31502077+ChanchalKumarMaji@users.noreply.github.com>
2018-12-10 19:41:26 +0530

    Codes Added

---
+++ 70. Climbing Stairs/main.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    int climbStairs(int n) {
+        if(n==0)
+            return 0;
+        if(n==1)
+            return 1;
+        if(n==2)
+            return 2;
+        
+        long int a=1,b=2,c;
+        for(int i=3;i<=n;i++)
+        {
+            c=a+b;
+            a=b;
+            b=c;
+            
+            
+        }
+    return c;
+    }
+};
