0d6bfcf4d090448869f89c9b21f2ec9a068e0005
Chanchal Kumar Maji <31502077+ChanchalKumarMaji@users.noreply.github.com>
2018-12-30 08:28:57 +0530

    Codes Added

---
+++ 101. Symmetric Tree/main.cpp
@@ -0,0 +1,22 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+class Solution {
+    bool check(TreeNode* t1, TreeNode* t2){
+        if(!t1 && !t2)
+            return true;
+        if(!t1 || !t2)
+            return false;
+        return t1->val == t2->val && check(t1->left, t2->right) && check(t1->right, t2->left); 
+    }
+public:
+    bool isSymmetric(TreeNode* root) {
+        return check(root, root);
+    }
+};
