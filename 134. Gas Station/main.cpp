2b00c95afafe41992b49be9c6acfae017b6b302b
Chanchal Kumar Maji <31502077+ChanchalKumarMaji@users.noreply.github.com>
2019-01-17 00:16:19 +0530

    Codes Added

---
+++ 134. Gas Station/main.cpp
@@ -0,0 +1,9 @@
+class Solution {
+public:
+    int canCompleteCircuit(vector<int> &gas, vector<int> &cost) {
+        int start(0),total(0),tank(0);
+        //if car fails at 'start', record the next station
+        for(int i=0;i<gas.size();i++) if((tank=tank+gas[i]-cost[i])<0) {start=i+1;total+=tank;tank=0;}
+        return (total+tank<0)? -1:start;
+    }
+};
