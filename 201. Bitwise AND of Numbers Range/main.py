5bbab84ec182f349a0f5b24d507435b048df5c6d
Chanchal Kumar Maji <31502077+ChanchalKumarMaji@users.noreply.github.com>
2019-02-13 09:48:22 +0530

    Codes Added

---
+++ 201. Bitwise AND of Numbers Range/main.py
@@ -0,0 +1,10 @@
+class Solution:
+    def rangeBitwiseAnd(self, m, n):
+        """
+        :type m: int
+        :type n: int
+        :rtype: int
+        """
+        while m<n:
+            n=n & (n-1)
+        return n
