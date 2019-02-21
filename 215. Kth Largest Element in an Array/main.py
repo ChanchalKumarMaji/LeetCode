d58ed4684332d211341faf7732986c03e4e08efa
Chanchal Kumar Maji <31502077+ChanchalKumarMaji@users.noreply.github.com>
2019-02-21 21:27:43 +0530

    Codes Added

---
+++ 215. Kth Largest Element in an Array/main.py
@@ -0,0 +1,9 @@
+class Solution:
+    def findKthLargest(self, nums, k):
+        """
+        :type nums: List[int]
+        :type k: int
+        :rtype: int
+        """
+        nums.sort()
+        return nums[::-1][k-1]
