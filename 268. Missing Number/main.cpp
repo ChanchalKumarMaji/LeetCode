250cc53be735455a02d3eaa889d5bc29c644c201
Chanchal Kumar Maji <31502077+ChanchalKumarMaji@users.noreply.github.com>
2019-03-13 01:12:16 +0530

    Codes Added

---
+++ 268. Missing Number/main.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int missingNumber(vector<int>& nums) {
+        
+        int res=nums.size();
+        int i=0;
+        for(int num:nums){
+            res ^=num;
+            res ^=i;
+            i++;
+        }
+        
+        return res;
+    }
+};
