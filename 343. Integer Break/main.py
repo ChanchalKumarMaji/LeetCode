e5b2ac7a017ec1a041715e5ed53ee11c8910e799
Chanchal Kumar Maji <31502077+ChanchalKumarMaji@users.noreply.github.com>
2019-03-29 17:37:12 +0530

    Codes Added

---
+++ 343. Integer Break/main.py
@@ -0,0 +1,16 @@
+class Solution:
+    def integerBreak(self, n):
+        """
+        :type n: int
+        :rtype: int
+        """
+        if n==2:
+            return 1
+        elif n==3:
+            return 2
+        elif n%3==0:
+            return pow(3,n//3)
+        elif n%3==1:
+            return pow(3,n//3 -1)*4
+        elif n%3==2:
+            return pow(3,n//3)*2
