be984ed6763daf1ab47d165b9d524b93b9f9ba88
Chanchal Kumar Maji <31502077+ChanchalKumarMaji@users.noreply.github.com>
2019-03-30 09:10:45 +0530

    Codes Added

---
+++ 344. Reverse String/main.cpp
@@ -0,0 +1,10 @@
+class Solution {
+public:
+    string reverseString(string s) {
+        int i=0,j=s.length()-1;
+        while(i<j){
+            swap(s[i++],s[j--]);
+        }
+        return s; 
+    }
+};
