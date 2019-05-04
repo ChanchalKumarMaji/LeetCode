a5e91bc428a06b22fae16785963e837fc83fd983
Chanchal Kumar Maji <31502077+ChanchalKumarMaji@users.noreply.github.com>
2019-05-04 08:29:02 +0530

    Codes Added

---
+++ 476. Number Complement/main.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    int findComplement(int num) {
+        int res=0,i=0;
+        while(num){
+            res=res+ (!(num & 1))*pow(2,i);
+            num >>= 1;
+            i++;
+        }
+        return res;
+    }
+};
