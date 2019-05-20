41e1c79937e10731cdc8597b82ab4779a4db6316
Chanchal Kumar Maji <31502077+ChanchalKumarMaji@users.noreply.github.com>
2019-05-21 01:42:47 +0530

    Codes Added

---
+++ 551. Student Attendance Record I/main.py
@@ -0,0 +1,10 @@
+class Solution:
+    def checkRecord(self, s):
+        """
+        :type s: str
+        :rtype: bool
+        """
+        if s.count('A')>1 or s.count('LLL')>0:
+            return False
+        
+        return True
