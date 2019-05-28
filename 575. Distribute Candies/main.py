0073f5d453c7e0c6e4e8366f09b7e4832766b50d
Chanchal Kumar Maji <31502077+ChanchalKumarMaji@users.noreply.github.com>
2019-05-28 11:23:52 +0530

    Codes Added

---
+++ 575. Distribute Candies/main.py
@@ -0,0 +1,8 @@
+class Solution:
+    def distributeCandies(self, candies):
+        """
+        :type candies: List[int]
+        :rtype: int
+        """
+        
+        return min(len(candies)//2,len(set(candies)))
