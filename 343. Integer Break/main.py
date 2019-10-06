4994d09615b56d21a2600c9d0c5d61ed2f3c3f33
Chanchal Kumar Maji <31502077+ChanchalKumarMaji@users.noreply.github.com>
2019-10-06 20:28:32 +0530

    Codes Added

--- 343. Integer Break/main.py
+++ 343. Integer Break/main.py
@@ -4,13 +4,13 @@ class Solution:
         :type n: int
         :rtype: int
         """
-        if n==2:
+        if n == 2:
             return 1
-        elif n==3:
+        elif n == 3:
             return 2
-        elif n%3==0:
+        elif n%3 == 0:
             return pow(3,n//3)
-        elif n%3==1:
+        elif n%3 == 1:
             return pow(3,n//3 -1)*4
-        elif n%3==2:
+        elif n%3 == 2:
             return pow(3,n//3)*2
