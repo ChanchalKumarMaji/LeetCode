9a4ac92e675762325d93ad4deb6f3bed20ecec1a
Chanchal Kumar Maji <31502077+ChanchalKumarMaji@users.noreply.github.com>
2019-10-11 16:34:51 +0530

    add problem-1137

---
+++ 1137. N-th Tribonacci Number/main.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int tribonacci(int n) {
+        if(n == 0) return 0;
+        if(n == 1) return 1;
+        if(n == 2) return 1;
+        int a = 0, b = 1, c = 1, d = 0;
+        for(int i=3; i<=n; i++){
+            d = a + b + c;
+            a = b;
+            b = c;
+            c = d;
+        }
+        return d;
+    }
+};
