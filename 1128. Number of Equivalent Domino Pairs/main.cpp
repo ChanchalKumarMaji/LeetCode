bc59c2cff1f850bf21746005b54361bb6aedd9a2
Chanchal Kumar Maji <31502077+ChanchalKumarMaji@users.noreply.github.com>
2019-11-17 10:33:50 +0530

    Codes Added

---
+++ 1128. Number of Equivalent Domino Pairs/main.cpp
@@ -0,0 +1,18 @@
+typedef pair<int, int> ii;
+class Solution {
+public:
+    int numEquivDominoPairs(vector<vector<int>>& dominoes) {
+        int res = 0;
+        map<ii, int> dict;
+        for(int i=0; i<dominoes.size(); i++){
+            int a = dominoes[i][0], b = dominoes[i][1];
+            ii key = {min(a, b), max(a, b)};
+            dict[key] += 1;
+        }
+        for(auto it = dict.begin(); it != dict.end(); it++){
+            int value = it->second;
+            res += value*(value-1) / 2;
+        }
+        return res;
+    }
+};
