12be5ae4b9ccd76290a77cfd8607448d2e2fc811
Chanchal Kumar Maji <31502077+ChanchalKumarMaji@users.noreply.github.com>
2020-01-12 00:11:41 +0530

    Codes Added

---
+++ 1313. Decompress Run-Length Encoded List/main.py
@@ -0,0 +1,7 @@
+class Solution:
+    def decompressRLElist(self, nums: List[int]) -> List[int]:
+        res = []
+        for i in range(1, len(nums), 2):
+            res += [nums[i]]*nums[i-1]
+        
+        return res
