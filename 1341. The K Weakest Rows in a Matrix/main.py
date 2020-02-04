5c20f6a8fd5c08916a45a744cc4b1c25a05e17dd
Chanchal Kumar Maji <31502077+ChanchalKumarMaji@users.noreply.github.com>
2020-02-04 09:57:18 +0530

    Codes Added

--- 1341. The K Weakest Rows in a Matrix/main.py
+++
@@ -1,8 +0,0 @@
-class Solution:
-    def kWeakestRows(self, mat: List[List[int]], k: int) -> List[int]:
-        res = []
-        m, n = len(mat), len(mat[0])
-        for i in range(m):
-            res.append((mat[i].count(1), i))
-        res.sort()
-        return [e[1] for e in res[:k]]
