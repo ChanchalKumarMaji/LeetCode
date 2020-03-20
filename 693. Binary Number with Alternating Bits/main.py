e752c96d7be41c9a186b6815712f076687765de7
Chanchal Kumar Maji <31502077+ChanchalKumarMaji@users.noreply.github.com>
2020-03-20 09:23:23 +0530

    Codes Added

--- 693. Binary Number with Alternating Bits/main.py
+++ 693. Binary Number with Alternating Bits/main.py
@@ -1,20 +1,18 @@
 class Solution {
 public:
     bool hasAlternatingBits(int n) {
-        int b=n & 1;
-        n=n>>1;
+        int b = n & 1;
+        n = n >> 1;
         int bb;
         
         while(n){
-            bb=n & 1;
-            n=n>>1;
-            if(b!=bb)
-                b=bb;
+            bb = n & 1;
+            n = n >> 1;
+            if(b != bb)
+                b = bb;
             else
                 return false;
-            
         }
-        
         return true;
     }
 };
