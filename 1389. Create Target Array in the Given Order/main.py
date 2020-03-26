fc022e59e49b18972a2e7b0c9eb5aeb8e0f1c8a8
Chanchal Kumar Maji <31502077+ChanchalKumarMaji@users.noreply.github.com>
2020-03-26 08:51:02 +0530

    Codes Added

---
+++ 1389. Create Target Array in the Given Order/main.py
@@ -0,0 +1,6 @@
+class Solution:
+    def createTargetArray(self, nums: List[int], index: List[int]) -> List[int]:
+        res = []
+        for i in range(len(nums)):
+            res = res[:index[i]] + [nums[i]] + res[index[i]:]
+        return res
