8cf6f8126f5b5518fed81e31295ece88c63dd907
Chanchal Kumar Maji <31502077+ChanchalKumarMaji@users.noreply.github.com>
2020-04-05 13:53:00 +0530

    Codes Added

--- 122. Best Time to Buy and Sell Stock II/main.cpp
+++ 122. Best Time to Buy and Sell Stock II/main.cpp
@@ -1,9 +1,9 @@
 class Solution {
 public:
     int maxProfit(vector<int>& prices) {
-        int profit=0,i;
+        int profit=0;
         
-        for(i=1;i<prices.size();i++)
+        for(int i=1;i<prices.size();i++)
             if(prices[i]>prices[i-1])
                 profit += prices[i]-prices[i-1];
 
