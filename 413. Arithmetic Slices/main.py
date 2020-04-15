1070e4864138e7a778d7c0313044ccf64e6fbd46
Chanchal Kumar Maji <31502077+ChanchalKumarMaji@users.noreply.github.com>
2020-04-15 19:56:01 +0530

    Codes Added

--- 413. Arithmetic Slices/main.py
+++ 413. Arithmetic Slices/main.py
@@ -4,16 +4,16 @@ class Solution:
         :type A: List[int]
         :rtype: int
         """
-        if len(A)<3:
+        if len(A) < 3:
             return 0
-        k=0
-        s=0
+        k = 0
+        s = 0
         A.append(0.1)
-        for i in range(2,len(A)):
-            if A[i-2]-A[i-1]==A[i-1]-A[i]:
-                k+=1
+        for i in range(2, len(A)):
+            if A[i-2]-A[i-1] == A[i-1]-A[i]:
+                k += 1
             else:
-                s=s+(k)*(k+1)//2
-                k=0
+                s = s+(k)*(k+1)//2
+                k = 0
                 
         return s
