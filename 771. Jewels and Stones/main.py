13c77d3a7f4782bbe7eba80eb137eb82f7615dbe
Chanchal Kumar Maji <31502077+ChanchalKumarMaji@users.noreply.github.com>
2020-06-13 19:37:24 +0530

    Codes Added

--- 771. Jewels and Stones/main.py
+++ 771. Jewels and Stones/main.py
@@ -1,4 +1,3 @@
-import collections
 class Solution:
     def numJewelsInStones(self, J, S):
         """
@@ -6,11 +5,10 @@ class Solution:
         :type S: str
         :rtype: int
         """
-        d=collections.defaultdict(int)
+        d = collections.defaultdict(int)
         for s in S:
-            d[s]+=1
-        sum=0
+            d[s] += 1
+        sum = 0
         for j in J:
-            sum+=d[j]
-            
+            sum += d[j]
         return sum
