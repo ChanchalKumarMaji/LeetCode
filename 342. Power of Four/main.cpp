4f60fafdb9cf252c2a1b1c0b7e5fa0880a1b6657
Chanchal Kumar Maji <31502077+ChanchalKumarMaji@users.noreply.github.com>
2020-07-03 08:04:35 +0530

    Codes Added

--- 342. Power of Four/main.cpp
+++ 342. Power of Four/main.cpp
@@ -1,19 +1,16 @@
 class Solution {
 public:
     bool isPowerOfFour(int n) {
-        int count=0;
-        if(n<=0)
+        int count = 0;
+        if(n <= 0)
             return false;
         if(n && !(n & (n-1))){
-            while(n>1){
-                n >>=1;
-                count+=1;
+            while(n > 1){
+                n >>= 1;
+                count += 1;
             }
-        return (count%2==0)?true:false;    
+        return (count % 2 == 0)?true:false;    
         }
-        
         return false;
-        
-        
     }
 };
