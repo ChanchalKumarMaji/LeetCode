d0f7b1cdb45fb594e03d48b848857224d9530f7c
Chanchal Kumar Maji <31502077+ChanchalKumarMaji@users.noreply.github.com>
2020-07-10 10:14:37 +0530

    Codes Added

--- 830. Positions of Large Groups/main.py
+++ 830. Positions of Large Groups/main.py
@@ -4,24 +4,19 @@ class Solution:
         :type S: str
         :rtype: List[List[int]]
         """
-        if len(S)==0:
+        if len(S) == 0:
             return []
-        res=[]
-        prev=S[0]
-        l=0
-        r=0
-        S=S+"#"
-        for i in range(1,len(S)):
-            if S[i]==prev:
-                r+=1
+        res = []
+        prev = S[0]
+        l = 0
+        r = 0
+        S = S + "#"
+        for i in range(1, len(S)):
+            if S[i] == prev:
+                r += 1
             else:
-                if r-l+1>=3:
-                    res.append([l,r])
-                prev=S[i]
-                l=r=i
-        
-        
-        
-        
-        
+                if r-l+1 >= 3:
+                    res.append([l, r])
+                prev = S[i]
+                l = r = i
         return res
